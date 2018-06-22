c09acd9016e30e7fc12200875779ba04e99d0a59
kingsznhone <35349300+kingsznhone@users.noreply.github.com>
2018-06-21 23:49:06 -0500

    hotfix

--- mouse click/mouse click/Form1.Designer.cs
+++ mouse click/mouse click/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -71,11 +72,21 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Ctrl + Shift +D 开始暴力连点";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "By: KingsZNHONE";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(380, 199);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -97,6 +108,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
